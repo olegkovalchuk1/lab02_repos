0499b2132111a6cb6ba5fb8b23ce2dc89b833a47
olegkovalchuk1 <olegkovalcuk993@gmail.com>
2025-09-29 21:23:33 +0300

    Add files via upload

---
+++ Lab_4_2/Lab_4_2.cpp
@@ -0,0 +1,50 @@
+// Lab_04_2.cpp
+// Ковальчук Олег
+// Лабораторна робота №4.2
+/*Табуляція функції, заданої
+формулою: функція однієї змінної*/
+//формулою: функція однієї змінної
+// Варіант 10
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x, xp, xk, dx, A, B, y;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+
+    cout << fixed;
+    cout << "-------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(12) << "y" << " |" << endl;
+    cout << "-------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk )  
+    {
+        A = 4.95 * x * x;
+
+        if (x < -3.5)
+            B = 4 + pow(x, -2);
+        else if (x < 1)
+            B = tan((3.5 + x) / 5.0);
+        else
+            B = sin(3 * x) - cos(x);
+
+        y = A + B;
+
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(12) << setprecision(5) << y
+            << " |" << endl;
+
+        x += dx;
+    }
+
+    cout << "-------------------------------" << endl;
+    return 0;
+}
